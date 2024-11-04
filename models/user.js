786e7c42a391514b597b5156658fc0092d6e918b
JstrR6 <tigerspires13@gmail.com>
2024-11-04 21:12:29 +0000

    Update

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    password: { type: String, required: true },
+    password: { type: String },
     discordId: { type: String, required: true },
     roles: { type: [String], default: [] },
     xp: { type: Number, default: 0 }
